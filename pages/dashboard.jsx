6c1c73dfd11fd682c6c842f56b16995bc63d95f6
SCSoftball21 <SCSoftball21@yahoo.com>
2026-08-30 22:40:22 -0500

    Update dashboard.jsx

--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import '../styles/dashboard-global.css';
 import styles from '../styles/dashboard.module.css';
 
 export default function Dashboard() {
